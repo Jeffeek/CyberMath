20080ded2ebe90bcef98f9683ead95438d8cd7ee
Mikhail Ilyushin <mishamine26@mail.ru>
2020-12-03 20:04:07 +0300

    Небольшой рефакторинг класса с методами расширения

--- CyberMath.Structures/CyberMath.Structures.Matrix/CyberMath.Structures.Matrix.Matrix/Extensions/ReferenceTypeMatrixExtensions.cs
+++ CyberMath.Structures/CyberMath.Structures.Matrix/CyberMath.Structures.Matrix.Matrix/Extensions/ReferenceTypeMatrixExtensions.cs
@@ -7,6 +7,10 @@ namespace CyberMath.Structures.Matrix.Matrix.Extensions
 {
     public static class ReferenceTypeMatrixExtensions
     {
+        #region Sum
+
+        #region int
+
         public static int Sum<TSource>(this Matrix<TSource> matrix, Func<TSource, int> selector)
         {
             if (matrix == null) throw new ArgumentException("Matrix is null!");
@@ -39,6 +43,10 @@ namespace CyberMath.Structures.Matrix.Matrix.Extensions
 
             return sum;
         }
+
+        #endregion
+
+        #region double
 
         public static double Sum<TSource>(this Matrix<TSource> matrix, Func<TSource, double> selector)
         {
@@ -73,6 +81,10 @@ namespace CyberMath.Structures.Matrix.Matrix.Extensions
             return sum;
         }
 
+        #endregion
+
+        #region decimal
+
         public static decimal Sum<TSource>(this Matrix<TSource> matrix, Func<TSource, decimal> selector)
         {
             if (matrix == null) throw new ArgumentException("Matrix is null!");
@@ -105,5 +117,106 @@ namespace CyberMath.Structures.Matrix.Matrix.Extensions
 
             return sum;
         }
+
+        #endregion
+
+        #endregion
+
+        #region DiagonalSum
+
+        #region int
+
+        public static int DiagonalSum<TSource>(this Matrix<TSource> matrix, Func<TSource, int> selector)
+        {
+            if (matrix == null) throw new ArgumentException("Matrix is null!");
+            if (selector == null) throw new ArgumentException("selector is null");
+            if (!matrix.IsSquare) throw new ArgumentException("Diagonal sum can be calculated only for square matrix (ColumnsCount = RowsCount)");
+            int sum = 0;
+            for (int i = 0; i < matrix.RowsCount; i++)
+            {
+                sum += selector(matrix[i, i]);
+            }
+            return sum;
+        }
+
+        public static int DiagonalSum<TSource>(this Matrix<TSource> matrix, Func<TSource, int?> selector)
+        {
+            if (matrix == null) throw new ArgumentException("Matrix is null!");
+            if (selector == null) throw new ArgumentException("selector is null");
+            if (!matrix.IsSquare) throw new ArgumentException("Diagonal sum can be calculated only for square matrix (ColumnsCount = RowsCount)");
+            int sum = 0;
+            for (int i = 0; i < matrix.RowsCount; i++)
+            {
+                if (matrix[i, i] != null)
+                    sum += selector(matrix[i, i]).Value;
+            }
+            return sum;
+        }
+
+        #endregion
+
+        #region double
+
+        public static double DiagonalSum<TSource>(this Matrix<TSource> matrix, Func<TSource, double> selector)
+        {
+            if (matrix == null) throw new ArgumentException("Matrix is null!");
+            if (selector == null) throw new ArgumentException("selector is null");
+            if (!matrix.IsSquare) throw new ArgumentException("Diagonal sum can be calculated only for square matrix (ColumnsCount = RowsCount)");
+            double sum = 0;
+            for (int i = 0; i < matrix.RowsCount; i++)
+            {
+                sum += selector(matrix[i, i]);
+            }
+            return sum;
+        }
+
+        public static double DiagonalSum<TSource>(this Matrix<TSource> matrix, Func<TSource, double?> selector)
+        {
+            if (matrix == null) throw new ArgumentException("Matrix is null!");
+            if (selector == null) throw new ArgumentException("selector is null");
+            if (!matrix.IsSquare) throw new ArgumentException("Diagonal sum can be calculated only for square matrix (ColumnsCount = RowsCount)");
+            double sum = 0;
+            for (int i = 0; i < matrix.RowsCount; i++)
+            {
+                if (matrix[i, i] != null)
+                    sum += selector(matrix[i, i]).Value;
+            }
+            return sum;
+        }
+
+        #endregion
+
+        #region decimal
+
+        public static decimal DiagonalSum<TSource>(this Matrix<TSource> matrix, Func<TSource, decimal> selector)
+        {
+            if (matrix == null) throw new ArgumentException("Matrix is null!");
+            if (selector == null) throw new ArgumentException("selector is null");
+            if (!matrix.IsSquare) throw new ArgumentException("Diagonal sum can be calculated only for square matrix (ColumnsCount = RowsCount)");
+            decimal sum = 0;
+            for (int i = 0; i < matrix.RowsCount; i++)
+            {
+                sum += selector(matrix[i, i]);
+            }
+            return sum;
+        }
+
+        public static decimal? DiagonalSum<TSource>(this Matrix<TSource> matrix, Func<TSource, decimal?> selector)
+        {
+            if (matrix == null) throw new ArgumentException("Matrix is null!");
+            if (selector == null) throw new ArgumentException("selector is null");
+            if (!matrix.IsSquare) throw new ArgumentException("Diagonal sum can be calculated only for square matrix (ColumnsCount = RowsCount)");
+            decimal sum = 0;
+            for (int i = 0; i < matrix.RowsCount; i++)
+            {
+                if (matrix[i, i] != null)
+                    sum += selector(matrix[i, i]).Value;
+            }
+            return sum;
+        }
+
+        #endregion
+
+        #endregion
     }
 }
