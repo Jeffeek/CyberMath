64a30da825db5461beb5a73defe7dd8830901462
Mikhail Ilyushin <ilyushin_misha@mail.ru>
2020-12-24 20:59:45 +0300

    Добавлена первичная реализация класса для квадратных уравнений + тесты

---
+++ CyberMath.Structures/CyberMath.Structures.Equations/CyberMath.Structures.Equations.QuadraticEquation/QuadraticEquation.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace CyberMath.Structures.Equations.QuadraticEquation
+{
+    public class QuadraticEquation
+    {
+        private readonly Regex _quadraticEquationPattern = new Regex("([+-]?\\d+|[+-]?\\d+\\.[\\d]{1,})[Xx]\\^2([+-]?\\d+|[+-]?\\d+\\.[\\d]{1,})[Xx]([+-]?\\d+|[+-]?\\d+\\.[\\d]{1,})");
+        
+        public double A { get; }
+        public double B { get; }
+        public double C { get; }
+        public double Determinant { get; private set; }
+        
+        //TODO: предусмотреть корни с мнимой частью
+        public double? FirstRoot { get; private set; }
+        public double? SecondRoot { get; private set; }
+
+        public QuadraticEquation(double a, double b, double c)
+        {
+            A = a;
+            B = b;
+            C = c;
+            CalculateDeterminant();
+        }
+
+        public QuadraticEquation(string quadraticEquation)
+        {
+            quadraticEquation = quadraticEquation.Replace(" ", String.Empty);
+            var match = _quadraticEquationPattern.Match(quadraticEquation);
+            if (!match.Success) throw new Exception("Input quadratic equation is not valid");
+            A = double.Parse(match.Groups[1].Value);
+            B = double.Parse(match.Groups[2].Value);
+            C = double.Parse(match.Groups[3].Value);
+            CalculateDeterminant();
+        }
+
+        private void CalculateDeterminant()
+        {
+            Determinant = Math.Pow(B, 2) - 4 * A * C;
+            if (Determinant < 0) return;
+            CalculateRoots();
+        }
+
+        private void CalculateRoots()
+        {
+            FirstRoot = (-B + Math.Sqrt(Determinant)) / (2 * A);
+            SecondRoot = (-B - Math.Sqrt(Determinant)) / (2 * A);
+        }
+    }
+}
---
+++ CyberMath.Tests/CyberMath.QuadraticEquation.Tests/PrimitiveCalculationsTests.cs
@@ -0,0 +1,56 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CyberMath.QuadraticEquation.Tests
+{
+    [TestClass]
+    public class PrimitiveCalculationsTests
+    {
+        [TestMethod]
+        public void QuadraticEquation_onNumbers()
+        {
+            int a = 5, b = -16, c = 12;
+            var equation = new Structures.Equations.QuadraticEquation.QuadraticEquation(a, b, c);
+            double expectedDeterminant = 16;
+            double actualDeterminant = equation.Determinant;
+            Assert.AreEqual(expectedDeterminant, actualDeterminant);
+            double expectedFirstRoot = 2d;
+            double actualFirstRoot = equation.FirstRoot.Value;
+            Assert.AreEqual(expectedFirstRoot, actualFirstRoot);
+            double expectedSecondRoot = 1.2d;
+            double actualSecondRoot = equation.SecondRoot.Value;
+            Assert.AreEqual(expectedSecondRoot, actualSecondRoot);
+        }
+
+        [TestMethod]
+        public void QuadraticEquation_stringParse_w0()
+        {
+            int a = 5, b = -16, c = 12;
+            var equation = new Structures.Equations.QuadraticEquation.QuadraticEquation("5x^2 -16x+12=0");
+            double expectedDeterminant = 16;
+            double actualDeterminant = equation.Determinant;
+            Assert.AreEqual(expectedDeterminant, actualDeterminant);
+            double expectedFirstRoot = 2d;
+            double actualFirstRoot = equation.FirstRoot.Value;
+            Assert.AreEqual(expectedFirstRoot, actualFirstRoot);
+            double expectedSecondRoot = 1.2d;
+            double actualSecondRoot = equation.SecondRoot.Value;
+            Assert.AreEqual(expectedSecondRoot, actualSecondRoot);
+        }
+
+        [TestMethod]
+        public void QuadraticEquation_stringParse_wo0()
+        {
+            int a = 5, b = -16, c = 12;
+            var equation = new Structures.Equations.QuadraticEquation.QuadraticEquation("5x^2 -16x  +12");
+            double expectedDeterminant = 16;
+            double actualDeterminant = equation.Determinant;
+            Assert.AreEqual(expectedDeterminant, actualDeterminant);
+            double expectedFirstRoot = 2d;
+            double actualFirstRoot = equation.FirstRoot.Value;
+            Assert.AreEqual(expectedFirstRoot, actualFirstRoot);
+            double expectedSecondRoot = 1.2d;
+            double actualSecondRoot = equation.SecondRoot.Value;
+            Assert.AreEqual(expectedSecondRoot, actualSecondRoot);
+        }
+    }
+}
